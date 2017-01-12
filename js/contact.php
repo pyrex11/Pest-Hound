bb727b64aa42e8df0cf8f88fb9f93cdafeafdfc5
pyrex11 <pyrexkoala@gmail.com>
2017-01-12 18:15:25 -0500

    Update contact.php

--- js/contact.php
+++ js/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Demo contact form <pesthound@gmail.com>'; 
-$sendTo = 'Demo contact form <pesthound@gmail.com>';
+$from = 'Demo contact form <pyrexkoala@gmail.com>'; 
+$sendTo = 'Demo contact form <pyrexkoala@gmail.com>';
 $subject = 'New message from PestHound.com';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
